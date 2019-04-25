05a0fb2aeb2e0c1f83345214b3f7ea57f4248a58
Alexis <alexisdrops@gmail.com>
2019-04-24 21:06:59 -0300

    created conponents

---
+++ src/components/InputNumbers.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="one">
+    <input type="number" placeholder="Digite o 1º número">
+    <input type="number" placeholder="Digite o 2º número">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputNumbers"
+};
+</script>
+
