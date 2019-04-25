dc3c0f1297af409bb3e3bc3ad301fb0f7c9ad789
Alexis <alexisdrops@gmail.com>
2019-04-24 21:07:55 -0300

    added unsalved file

--- src/components/InputNumbers.vue
+++ src/components/InputNumbers.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="one">
-    <input type="number" placeholder="Digite o 1º número">
-    <input type="number" placeholder="Digite o 2º número">
+    <input type="number" :value='one' @blur='pegar_one' placeholder="Digite o 1º número">
+    <input type="number" :value='two' @blur='pegar_two' placeholder="Digite o 2º número">
   </div>
 </template>
 
@@ -9,5 +9,13 @@
 export default {
   name: "InputNumbers"
 };
+
+data(){
+    methods: {
+        pegar_one(event){
+            
+        }
+    }
+}
 </script>
 
